43e7d8a23b36af479757ed3916aa730fecceb23c
Kwok Chun Ho <kch062522@gmail.com>
2020-10-16 17:58:35 +0800

    fix: `diane`'s img path

--- js/members.js
+++ js/members.js
@@ -378,7 +378,7 @@ const members = {
         {   name: '程筱珺',
             alias: 'diane',
             research: '#',
-            img: 'img/diane.jpg',
+            img: 'img/diane.JPG',
             color: 'blue',
             intro: 'ʕ•ᴥ•ʔ',
             link: '',
